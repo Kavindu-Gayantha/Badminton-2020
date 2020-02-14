7285fcb73dfcaaaf2081b0f462f9fd9972da92b9
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-02-14 07:59:19 +0530

    added ionic navigation bar in grid

---
+++ badminton/src/pages/Home.tsx
@@ -0,0 +1,36 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
+import React from 'react';
+import { IonGrid, IonRow, IonCol} from '@ionic/react';
+
+const Home: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Ionic Blank</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent className="ion-padding">
+        {/* The world is your oyster.
+        <p>
+          If you get lost, the{' '}
+          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
+            docs
+          </a>{' '}
+          will be your guide.
+        </p> */}
+      <IonGrid className="ion-grid-padding">
+          <IonRow className="ion-align-items-center">
+            <IonCol>profile pic is here</IonCol>
+            <IonCol><IonButton href="#" color="primary">Stats</IonButton> </IonCol>
+            <IonCol><IonButton href="#" color="primary">Team</IonButton> </IonCol>
+            <IonCol><IonButton href="#" color="primary">Overall</IonButton> </IonCol>
+          </IonRow>
+      </IonGrid>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
