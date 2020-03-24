002689c583caf945a80ca9bfb8e008eb2e358194
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-03-24 21:42:36 +0530

    first

--- badminton/lib/main.dart
+++ badminton/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 
 void main(){
@@ -9,25 +9,29 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'stateless widget',
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(700, 880, 800, 800),
-          title: Text('Badminton'),
-          leading: IconButton(icon: Icon(Icons.favorite), onPressed: null),
-        ),
-        
-        body: Container(
-          color:Colors.green,
-          child:Center(
-            // Text('welcome to badminton attendance marking app'),
-           child:Text('Welcome to Badminton attendance marking App')
-           ),
-              
-        ),
+      title:'badminton attendance marking app',
+      color: Colors.green,
+      home:( 
+      Center(
+        child:Column(
+          children: <Widget>[
+          Text('WELCOME to Badminton attendance marking App'),
+          RaisedButton(
+            onPressed: null,
+            child:Center(
+              child:Text('Go to App'),
+            ),
+          )
+
+        ],
+        ) 
+        )
       ),
-          
-    );      
-   
+      
+      
+
+      
+    
+    );
   }
 }
