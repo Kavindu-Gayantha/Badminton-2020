24e770bea427f7b39a2d18b4a3bc84a7e5bbdb60
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-03-26 16:22:35 +0530

    remade the first screen with exit button and go to app

--- badminton2020/lib/UI/welcome.dart
+++ badminton2020/lib/UI/welcome.dart
@@ -5,18 +5,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Material(
       color: Colors.greenAccent,
-      child:new Center( 
-        child:new Text(
-            "welcome UOK Badminton",
-            textDirection:TextDirection.ltr,
-            style:new TextStyle(
-              fontWeight:FontWeight.bold,
-              fontStyle:FontStyle.italic,
-              fontSize: 30.1,
-            
-            )
+      child:new Column( 
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        
+        children: <Widget>[
+          Text('Welcome'),
+          Text('To'),
+          Text('UOK BADMINTON-2020'),
+          Text('Attendance marking APP'),
+          RaisedButton(
+            onPressed: null,
+            child:Text('GO to APP'),
+            textTheme: ButtonTextTheme.accent,
+            textColor: Colors.blue,
+            ),
+          
+          Text('or'),
+          RaisedButton(onPressed: null,
+            child: Text('CLOSE APP'),
+            textColor: Colors.redAccent,
           )
-        ),
+         
+        ],
+      ),
+
+
+
+
+        // child:new Text(
+        //     "welcome UOK Badminton",
+        //     textDirection:TextDirection.ltr,
+        //     style:new TextStyle(
+        //       fontWeight:FontWeight.bold,
+        //       fontStyle:FontStyle.italic,
+        //       fontSize: 30.1,
+            
+        //     )
+        //   )
+        // ),
 
 
       );
