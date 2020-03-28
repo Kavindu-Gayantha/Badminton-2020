e8f9c2d91b727c62c999382cdbdbcff1bb53a75d
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-03-28 14:52:39 +0530

    added stats tab

---
+++ badminton2020/lib/UI/Tabs/TabStats/tabstats.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class tab_stats extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Center(child: Text('tab stats'),
+      )
+      
+    );
+  }
+}
