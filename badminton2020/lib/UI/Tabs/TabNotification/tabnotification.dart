012bdff499be5c83af5af25ec3c72325c82836ed
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-08 23:58:29 +0530

    added tab notification tab file

--- badminton2020/lib/UI/Tabs/TabNotification/tabnotification.dart
+++ badminton2020/lib/UI/Tabs/TabNotification/tabnotification.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class tab_notification extends StatelessWidget {
+class TabNotification extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child:Center(child: Text('notification',textDirection: TextDirection.ltr),
+      child:Center(child: Text('notification heheeh',textDirection: TextDirection.ltr),
       )
       
     );
