ccb69d1015879c4ef980d4f4bce7cf1ad8409ca2
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-08 23:38:39 +0530

    created new tabs tab girls and tab boys

---
+++ badminton2020/lib/UI/Tabs/TabGirls/tabGirls.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/cupertino.dart';
+
+class TabGirls extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Center(
+        child: Center(
+          child: Text('tab girls',textDirection: TextDirection.ltr  // bottomNavigationBar: BottomNavigationBar( 
+      //   backgroundColor: Colors.green.shade100,
+      //   onTap:onTabTapped,
+      //   type: BottomNavigationBarType.shifting,
+      //   currentIndex:_currentIndex,
+      //   items: const<BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Boys Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Girls Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.notifications),
+      //     title: Text('Notification',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.star),
+      //     title: Text('Show Stats',textDirection: TextDirection.ltr,),
+      //     )
+          
+      //   ],
+      //   // currentIndex: _selectedIndex,
+      //   selectedItemColor:Colors.greenAccent.shade700,
+       
+        
+      // ),  // bottomNavigationBar: BottomNavigationBar( 
+      //   backgroundColor: Colors.green.shade100,
+      //   onTap:onTabTapped,
+      //   type: BottomNavigationBarType.shifting,
+      //   currentIndex:_currentIndex,
+      //   items: const<BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Boys Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Girls Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.notifications),
+      //     title: Text('Notification',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.star),
+      //     title: Text('Show Stats',textDirection: TextDirection.ltr,),
+      //     )
+          
+      //   ],
+      //   // currentIndex: _selectedIndex,
+      //   selectedItemColor:Colors.greenAccent.shade700,
+       
+        
+      // ),),
+        ),
+      )
+      
+    ),
+    );
+  }
+}
