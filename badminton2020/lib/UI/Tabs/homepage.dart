0670c577002f6169eeb1b353c969297149cf28de
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-08 23:59:39 +0530

    modified bottom navigation bar

--- badminton2020/lib/UI/Tabs/homepage.dart
+++ badminton2020/lib/UI/Tabs/homepage.dart
@@ -2,11 +2,15 @@
 // import 'package:badminton2020/UI/Tabs/TabHome/tabhome.dart';
 
 import 'package:badminton2020/UI/LoginScreen/login.dart';
-import 'package:badminton2020/UI/Tabs/TabHome/tabhome.dart';
+import 'package:badminton2020/UI/Tabs/TabBoys/tabBoys.dart';
+// import 'package:badminton2020/UI/Tabs/TabGirls/tabBoys.dart';
+import 'package:badminton2020/UI/Tabs/TabGirls/tabGirls.dart';
+// import './/TabHome/tabhome.dart';
 import 'package:badminton2020/UI/Tabs/TabNotification/tabnotification.dart';
 import 'package:badminton2020/UI/Tabs/TabStats/tabstats.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import './../placeholder_widget.dart';
+// import './../placeholder_widget.dart';
 
 
 class welcomescreen extends StatefulWidget {
@@ -16,11 +20,11 @@ class welcomescreen extends StatefulWidget {
 
 class _welcomescreenState extends State<welcomescreen> {
    int _currentIndex = 0;
-  final List<Widget> _children = [
-    // placeholder_widget(),
-    // placeholder_widget(),
-    // placeholder_widget(),
-  ];
+  // final List<Widget> _children = [
+  //   // placeholder_widget(),
+  //   // placeholder_widget(),
+  //   // placeholder_widget(),
+  // ];
 
   // int _selectedIndex=0;
   // static const TextStyle optionStyle=
@@ -63,10 +67,18 @@ class _welcomescreenState extends State<welcomescreen> {
   //   }
    
   // }
-  void onTabTapped (int index){
-    setState(() {
-      _currentIndex=index;
-    });
+  // void onTabTapped (int index){
+  //   setState(() {
+  //     _currentIndex=index;
+  //   });
+  // }
+  int _currentTab=0;
+  PageController  _pageController;
+
+  @override
+  void initState(){
+    super.initState();
+    _pageController  =  PageController();
   }
 
   @override
@@ -89,7 +101,15 @@ class _welcomescreenState extends State<welcomescreen> {
                 )
                 
                 ),
-                
+                 // void onTabTapped (int index){
+  //   setState(() {
+  //     _currentIndex=index;
+  //   });
+  // } // void onTabTapped (int index){
+  //   setState(() {
+  //     _currentIndex=index;
+  //   });
+  // }
                 
               child: Text('Login here',
               style: TextStyle(
@@ -129,32 +149,84 @@ class _welcomescreenState extends State<welcomescreen> {
         ),
       ),
       
-      bottomNavigationBar: BottomNavigationBar( 
-        backgroundColor: Colors.green.shade100,
-        onTap:onTabTapped,
-        type: BottomNavigationBarType.shifting,
-        currentIndex:_currentIndex,
-        items: const<BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.home),
-          title: Text('Boys Team',textDirection: TextDirection.ltr,),
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.home),
-          title: Text('Girls Team',textDirection: TextDirection.ltr,),
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.notifications),
-          title: Text('Notification',textDirection: TextDirection.ltr,),
-          ),
-          BottomNavigationBarItem(icon: Icon(Icons.star),
-          title: Text('Show Stats',textDirection: TextDirection.ltr,),
-          )
+      // bottomNavigationBar: BottomNavigationBar( 
+      //   backgroundColor: Colors.green.shade100,
+      //   onTap:onTabTapped,
+      //   type: BottomNavigationBarType.shifting,
+      //   currentIndex:_currentIndex,
+      //   items: const<BottomNavigationBarItem>[
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Boys Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.home),
+      //     title: Text('Girls Team',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.notifications),
+      //     title: Text('Notification',textDirection: TextDirection.ltr,),
+      //     ),
+      //     BottomNavigationBarItem(icon: Icon(Icons.star),
+      //     title: Text('Show Stats',textDirection: TextDirection.ltr,),
+      //     )
           
-        ],
-        // currentIndex: _selectedIndex,
-        selectedItemColor:Colors.greenAccent.shade700,
+      //   ],
+      //   // currentIndex: _selectedIndex,
+      //   selectedItemColor:Colors.greenAccent.shade700,
        
         
-      ),
-     
+      // ),
+    body: PageView(
+      controller: _pageController,
+      children: <Widget>[
+        TabBoys(),
+        TabGirls(),
+        TabNotification(),
+        TabStats(),
+      ],
+      onPageChanged: (int index){
+        setState(() {
+          _currentTab=index;
+        });
+      },
+    ),
+     bottomNavigationBar: CupertinoTabBar(
+       currentIndex:_currentTab,
+       onTap:(int index){
+         setState((){
+           _currentTab =index;
+         });
+         _pageController.animateToPage(index,
+          duration:Duration(microseconds: 2000) ,
+           curve: Curves.easeIn
+           );
+           Colors.green;
+          //  items: <BottomNavigationBarItem>[
+             
+
+          //  ];
+       }, items: <BottomNavigationBarItem>[
+         BottomNavigationBarItem(
+               icon: Icon(Icons.adb,size:30.0
+               ),
+               title:Text('Boys'),
+             ),
+             BottomNavigationBarItem(
+               icon: Icon(Icons.pregnant_woman,size:30.0
+             ),
+             title: Text('Girls'),
+             ),
+              BottomNavigationBarItem(
+               icon: Icon(Icons.notifications_active,size:30.0
+             ),
+             title: Text('Notification'),
+             ),
+              BottomNavigationBarItem(
+               icon: Icon(Icons.stars,size:30.0
+             ),
+             title: Text('Stats')
+             ),
+
+       ],
+     ),
     );
   }
 }
