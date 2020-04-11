f0a6a891e6a9ccfd9ee36d57d8cb4830c6ed29ab
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-11 09:26:00 +0530

    added different contact list for boys tab

--- badminton2020/lib/UI/Tabs/TabBoys/tabBoys.dart
+++ badminton2020/lib/UI/Tabs/TabBoys/tabBoys.dart
@@ -1,73 +1,42 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../contactlist/contact_list.dart';
+import '../contactlist/modal/contact.dart';
 
 class TabBoys extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    
-        return Card(
-          
-          child: ListView(
-            padding: EdgeInsets.all(10.0),
-          children: <Widget>[
-            Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-                 Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-                 Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-             Text('one'),
-            Text('two'),
-            
 
-          ],
-          ),
-        );
-      }
+  _buildContactlist() {
+
+        return <ContactModal>[
+              const ContactModal(
+                  fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+              const ContactModal(
+                  fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+              const ContactModal(
+                  fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+              const ContactModal(
+                  fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+              const ContactModal(
+                  fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+              const ContactModal(
+                  fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+              const ContactModal(
+                  fullName: 'Romain Hoogmoed', email: 'romain.hoogmoed@example.com'),
+              const ContactModal(
+                  fullName: 'Emilie Olsen', email: 'emilie.olsen@example.com'),
+              const ContactModal(
+                  fullName: 'Nishant Srivastava', email: 'nishant.srivastava@example.com'),
+            ];
+
+    
+  }
+    @override
+    Widget build(BuildContext context) {
+      
+          return Scaffold(body: ContactsList(_buildContactlist()),);
+                    
+                
     }
+          
+          
+}
