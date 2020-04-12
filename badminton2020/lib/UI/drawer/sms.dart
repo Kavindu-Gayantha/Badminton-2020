e0ad16d3f15dbeb2314427d62ae87bad18d0f7a8
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-13 03:57:50 +0530

    added sms class

---
+++ badminton2020/lib/UI/drawer/sms.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sms extends StatefulWidget {
+  @override
+  _smsState createState() => _smsState();
+}
+
+class _smsState extends State<sms> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('sms'),
+    );
+  }
+}
