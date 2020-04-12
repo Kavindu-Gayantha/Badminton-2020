323ecc6ef13f2edba04364c692b762731006f2d3
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-04-13 03:47:48 +0530

    addded settings clsss

---
+++ badminton2020/lib/UI/drawer/settings.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class settings extends StatefulWidget {
+  @override
+  _settingsState createState() => _settingsState();
+}
+
+class _settingsState extends State<settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        
+        Text('this is setttingss')
+        
+      ),
+    );
+  }
+}
